31686c1264eb2a664d7384f60391a59697343c8a
Karan Nandkar <knandkar007@gmail.com>
2023-07-10 13:49:25 +0530

    Initial commit

---
+++ binary_search.py
@@ -0,0 +1,25 @@
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last) // 2
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+
+
+def verify(result):
+    print("Target found at index: ", result)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = binary_search(numbers, 12)
+verify(result)
+
+result = binary_search(numbers, 9)
+verify(result)
